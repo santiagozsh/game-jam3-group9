7fd4028e7be66ee64d2f1af1424d260c36dba453
Juan Hernández <juanhernandeznz@gmail.com>
2024-04-05 12:15:10 -0500

    Add particles and fix movement

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,41 +1,23 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     private Collision coll;
-    [HideInInspector]
     public Rigidbody2D rb;
+    public SpriteRenderer sprite;
+    public Animator anim;
 
-    [Space]
-    [Header("Stats")]
     public float speed = 10;
     public float jumpForce = 50;
-    public float slideSpeed = 5;
-    public float wallJumpLerp = 10;
-    public float dashSpeed = 20;
 
-    [Space]
-    [Header("Booleans")]
     public bool canMove;
-    public bool wallGrab;
-    public bool wallJumped;
-    public bool wallSlide;
     public bool isDashing;
+    public bool isJumping;
+    public bool isFalling;
 
-    [Space]
+    public ParticleSystem dustParticle;
 
-    private bool groundTouch;
-    private bool hasDashed;
-    SpriteRenderer sprite;
-    Animator anim;
-    public int side = 1;
-
-
-
-    void Start()
+    private void Start()
     {
         coll = GetComponent<Collision>();
         rb = GetComponent<Rigidbody2D>();
@@ -43,151 +25,76 @@ public class Movement : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-
-    void Update()
+    private void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        Vector2 dir = new Vector2(x, y);
-
+        Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         Walk(dir);
+        anim.SetBool("HorizontalMove", dir.x != 0);
 
-        if (x != 0)
-            anim.SetBool("HorizontalMove", true);
-        else
-            anim.SetBool("HorizontalMove", false);
-
-
-        if (coll.onWall && Input.GetButton("Fire3") && canMove)
+        if (coll.onGround)
         {
-            if (side != coll.wallSide)
-                // anim.Flip(side * -1);
-                wallGrab = true;
-            wallSlide = false;
+            GetComponent<BetterJumping>().enabled = true;
+            isJumping = false;
+            isFalling = false;
         }
 
-        if (Input.GetButtonUp("Fire3") || !coll.onWall || !canMove)
+        if (Input.GetKeyDown(KeyCode.Space) && coll.onGround)
         {
-            wallGrab = false;
-            wallSlide = false;
+            
+            Jump(Vector2.up);
+            isJumping = true;
+            anim.SetBool("isJumping", isJumping);
+            isFalling = false; 
+            anim.SetBool("isFalling", isFalling);
+        }
+
+        if (rb.velocity.y > 3f)
+        {
+            isJumping = true;
+            isFalling = false;
+            anim.SetBool("isFalling", isFalling);
+            anim.SetBool("isJumping", isJumping);
+        }
+        else if (rb.velocity.y < -3f)
+        {
+            isFalling = true;
+            isJumping = false;
+            anim.SetBool("isFalling", isFalling);
+            anim.SetBool("isJumping", isJumping);
+        }
+        else
+        {
+            isJumping = false;
+            isFalling = false;
+            anim.SetBool("isJumping", isJumping);
+            anim.SetBool("isFalling", isFalling);
         }
 
         if (coll.onGround && !isDashing)
-        {
-            wallJumped = false;
-            GetComponent<BetterJumping>().enabled = true;
-        }
+            isDashing = false;
 
-        if (wallGrab && !isDashing)
-        {
-            rb.gravityScale = 0;
-            if (x > .2f || x < -.2f)
-                rb.velocity = new Vector2(rb.velocity.x, 0);
-
-            float speedModifier = y > 0 ? .5f : 1;
-
-            rb.velocity = new Vector2(rb.velocity.x, y * (speed * speedModifier));
-        }
-        else
-        {
-            rb.gravityScale = 3;
-        }
-
-        if (coll.onWall && !coll.onGround)
-        {
-            if (x != 0 && !wallGrab)
-            {
-                wallSlide = true;
-            }
-        }
-
-        if (!coll.onWall || coll.onGround)
-            wallSlide = false;
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (coll.onGround)
-            {
-                anim.SetTrigger("Jump");
-                Jump(Vector2.up);
-            }
-
-        }
-
-        if (Input.GetButtonDown("Fire1") && !hasDashed)
-        {
-            //if (xRaw != 0 || yRaw != 0)
-            // Dash(xRaw, yRaw);
-        }
-
-        if (coll.onGround && !groundTouch)
-        {
-            GroundTouch();
-            groundTouch = true;
-        }
-
-        if (!coll.onGround && groundTouch)
-        {
-            groundTouch = false;
-        }
-
-        //WallParticle(y);
-
-        if (wallGrab || wallSlide || !canMove)
-            return;
-
-        if (x > 0)
+        if (dir.x > 0)
         {
             sprite.flipX = true;
         }
-        if (x < 0)
+        if (dir.x < 0)
         {
             sprite.flipX = false;
         }
     }
 
-    void GroundTouch()
-    {
-
-        hasDashed = false;
-        isDashing = false;
-    }
-
-
     private void Walk(Vector2 dir)
     {
         if (!canMove)
             return;
-
-        if (wallGrab)
-            return;
-
-        if (!wallJumped)
-        {
-            rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
-        }
-        else
-        {
-            rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * speed, rb.velocity.y)), wallJumpLerp * Time.deltaTime);
-        }
+        rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+        
     }
 
     private void Jump(Vector2 dir)
     {
         rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.velocity += dir * jumpForce;
+        dustParticle.Play();
     }
-
-    IEnumerator DisableMovement(float time)
-    {
-        canMove = false;
-        yield return new WaitForSeconds(time);
-        canMove = true;
-    }
-
-    void RigidbodyDrag(float x)
-    {
-        rb.drag = x;
-    }
-
 }
